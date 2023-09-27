af2323ec4c8eeed3e3254f804c81320b04fa697a
benisprlh <benisaprulah5@gmail.com>
2023-09-27 14:44:26 +0700

    ini data game

---
+++ scripts/dataGame.js
@@ -0,0 +1,252 @@
+let dataGame = [
+    {
+        nama: "Nba",
+        genre: "Sports",
+        image: "https://blog.logomyway.com/wp-content/uploads/2017/01/nba-logo-design.jpg",
+        umur: 16,
+        harga: 490_000,
+        stok: 20
+    },
+    {
+        nama: "Fortnite",
+        genre: "Battle-Royal",
+        image: "https://1000logos.net/wp-content/uploads/2020/06/Fortnite-Logo-1.jpg",
+        umur: 19,
+        harga: 700_000,
+        stok: 39
+    },
+    {
+        nama: "PUBG",
+        genre: "Battle-Royal",
+        image: "https://cdns.klimg.com/bola.net/library/upload/21/2018/10/pubg-l_86a2a84.jpg",
+        umur: 19,
+        harga: 340_000,
+        stok: 46
+    },
+    {
+        nama: "FIFA",
+        genre: "Sports",
+        image: "https://www.fifplay.com/img/public/fifa-23-logo.jpg",
+        umur: 16,
+        harga: 800_000,
+        stok: 19
+    },
+    {
+        nama: "GTA V",
+        genre: "Action",
+        image: "https://seeklogo.com/images/G/grand-theft-auto-5-logo-54E30512F6-seeklogo.com.png",
+        umur: 19,
+        harga: 1_000_000,
+        stok: 12
+    },
+    {
+        nama: "SMACKDOWN WWE",
+        genre: "Sports",
+        image: "https://static.wikia.nocookie.net/logopedia/images/c/cd/48122316.jpg/revision/latest?cb=20150926123311",
+        umur: 19,
+        harga: 580_000,
+        stok: 10
+    },
+    {
+        nama: "NFS MOST WANTED",
+        genre: "Sports",
+        image: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/need-for-speed-most-wanted-2005/f/fe/NFS-most-wanted-for-pc.jpg",
+        umur: 15,
+        harga: 430_000,
+        stok: 11
+    },
+    {
+        nama: "CARS",
+        genre: "Sports",
+        image: "https://cdn.freebiesupply.com/logos/thumbs/2x/disney-and-pixar-cars-logo.png",
+        umur: 12,
+        harga: 140_000,
+        stok: 5
+    },
+    {
+        nama: "God Of War Ragnarok",
+        genre: "Action",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjjZv4j2AK-YvAWhFD2LZwibcY47PEvcdFsJYekwRFLgLLSkvqRutuWFEf1aR3zsPGJk0&usqp=CAU",
+        umur: 16,
+        harga: 640_000,
+        stok: 24
+    },
+    {
+        nama: "Mortal Kombat 11",
+        genre: "Action",
+        image: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f05a9b5b-ead5-460e-8573-73ba2fff9cde/dcxe28l-8348b34c-05da-43d5-875d-e83737b33589.png/v1/fill/w_1600,h_1599/mortal_kombat_11_dragon_logo_png_by_chanxgg_dcxe28l-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU5OSIsInBhdGgiOiJcL2ZcL2YwNWE5YjViLWVhZDUtNDYwZS04NTczLTczYmEyZmZmOWNkZVwvZGN4ZTI4bC04MzQ4YjM0Yy0wNWRhLTQzZDUtODc1ZC1lODM3MzdiMzM1ODkucG5nIiwid2lkdGgiOiI8PTE2MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.3BKLNgZKbsZCfzc-AByIWvfEnoVBV6ONLaZ33eHjbdk",
+        umur: 16,
+        harga: 549_000,
+        stok: 32
+    },
+    {
+        nama: "Cyberpunk 2077",
+        genre: "Action",
+        image: "https://static.tweaktown.com/news/8/8/88348_2077_cyberpunk-2077-is-only-getting-one-expansion-and-it-might-be-expensive_full.jpg",
+        umur: 20,
+        harga: 840_000,
+        stok: 3
+    },
+    {
+        nama: "Call Of Duty Modern Warfare III",
+        genre: "Battle-Royal",
+        image: "https://staticg.sportskeeda.com/editor/2023/07/0d1c2-16895879602139-1920.jpg?w=840",
+        umur: 19,
+        harga: 320_000,
+        stok: 6
+    },
+    {
+        nama: "Resident Evil Village",
+        genre: "Action",
+        image: "https://static.wikia.nocookie.net/capcomdatabase/images/7/78/REVillage_logo.png/revision/latest?cb=20221101174852",
+        umur: 18,
+        harga: 260_000,
+        stok: 22
+    },
+    {
+        nama: "Apex Legends",
+        genre: "Battle-Royal",
+        image: "https://seeklogo.com/images/A/apex-logo-F74B0C9FCD-seeklogo.com.png",
+        umur: 18,
+        harga: 900_000,
+        stok: 13
+    },
+    {
+        nama: "Smash Legends",
+        genre: "Battle-Royal",
+        image: "https://static.wikia.nocookie.net/gamia_gamepedia_en/images/3/3f/SMASH_LEGENDS.jpg/revision/latest?cb=20210502195532",
+        umur: 15,
+        harga: 120_000,
+        stok: 24
+    },
+    {
+        nama: "Bomber Grounds",
+        genre: "Battle-Royal",
+        image: "https://cdn.akamai.steamstatic.com/steam/apps/1104450/capsule_616x353.jpg?t=1665042084",
+        umur: 16,
+        harga: 210_000,
+        stok: 18
+    },
+    {
+        nama: "Totally Accurate Battlegrounds",
+        genre: "Battle-Royal",
+        image: "https://cdn.akamai.steamstatic.com/steam/apps/823130/capsule_616x353.jpg?t=1620818743",
+        umur: 16,
+        harga: 170_000,
+        stok: 16
+    },
+    {
+        nama: "PUBG",
+        genre: "Battle-Royal",
+        image: "https://cdns.klimg.com/bola.net/library/upload/21/2018/10/pubg-l_86a2a84.jpg",
+        umur: 19,
+        harga: 340_000,
+        stok: 46
+    },
+    {
+        nama: "Metal Slug",
+        genre: "Action",
+        umur: 14,
+        harga: 100_000,
+        stok : 49
+    },
+        
+    {
+        nama: "Downhill",
+        genre: "Sports",
+        umur: 14,
+        harga: 100_000,
+        stok : 49
+    },
+    {
+        nama: "God of War",
+        genre: "Action",
+        umur: 14,
+        harga: 400_000,
+        stok : 49
+     },
+    {
+        nama: "Assassin's Creed",
+        genre: "Action",
+        umur: 14,
+        harga: 300_000,
+        stok : 49
+    },
+    {
+        nama: "Dragon Ball's",
+        genre: "Action",
+        umur: 14,
+        harga: 100_000,
+        stok : 49
+    },
+    {
+        nama: "The Crew Motorfest",
+        genre: "Sports",
+        image : "https://upload.wikimedia.org/wikipedia/en/e/ea/The_Crew_Motorfest_cover_art.jpg",
+        umur: 14,
+        harga: 100_000,
+        stok : 19
+    },
+    {
+        nama: "Dragon Ball's",
+        genre: "Action",
+        umur: 14,
+        harga: 100_000,
+        stok : 49
+    },
+    {
+        nama: "Dragon Ball's",
+        genre: "Action",
+        umur: 14,
+        harga: 100_000,
+        stok : 49
+    },
+    {
+        nama: "Dragon Ball's",
+        genre: "Action",
+        umur: 14,
+        harga: 100_000,
+        stok : 49
+    },
+    {
+        nama: "Dragon Ball's",
+        genre: "Action",
+        umur: 14,
+        harga: 100_000,
+        stok : 49
+    },
+    {
+        nama: "Dragon Ball's",
+        genre: "Action",
+        umur: 14,
+        harga: 100_000,
+        stok : 49
+    },
+    {
+        nama: "Dragon Ball's",
+        genre: "Action",
+        umur: 14,
+        harga: 100_000,
+        stok : 49
+    }  
+]
+let countSports = 0;
+let countAction = 0;
+let countBR = 0;
+let countDewasa = 0;
+let countRemaja = 0;
+for (let i = 0; i < dataGame.length; i++){
+    if (dataGame[i].genre === 'Sports'){
+        countSports++;
+    } else if (dataGame[i].genre === 'Action'){
+        countAction++;
+    } else {
+        countBR++;
+    }
+    if (dataGame[i].umur < 18){
+        countRemaja++;
+    } else{
+        countDewasa++;
+    }
+}
+console.log(countSports, countAction, countBR, countRemaja, countDewasa);
